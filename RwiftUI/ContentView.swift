28d4ddc82b4c35254e8ebe3c2c7a4ceb93b626d3
Thaw De Zin <thawde.zin@brycenmyanmar.com.mm>
2025-06-10 16:10:59 +0630

    Initial Commit

---
+++ RwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RwiftUI
+//
+//  Created by thawdezin on 6/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RwiftUI/RwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RwiftUIApp.swift
+//  RwiftUI
+//
+//  Created by thawdezin on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
